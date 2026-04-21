a7d0acabc001fcd1a0bd5ce6405127d22305326d
murugannagaraja781 <murugannagaraja781@gmail.com>
2026-04-21 19:52:35 +0530

    Update admin.routes.js

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -4,8 +4,8 @@ const adminController = require('../controllers/admin.controller');
 
 router.get('/academy/videos', adminController.getVideos);
 router.post('/academy/videos', adminController.addVideo);
-router.get('/banners', adminController.getBanners);
-router.post('/banners', adminController.addBanner);
+// router.get('/banners', adminController.getBanners);
+// router.post('/banners', adminController.addBanner);
 router.get('/deletion-requests', adminController.getDeletionRequests);
 router.post('/process-deletion', adminController.processDeletion);
 router.post('/update-balance', adminController.updateBalance);

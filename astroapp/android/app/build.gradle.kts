89df776125e8579be6e3e20475a88e6818b29fb3
murugannagaraja781 <murugannagaraja781@gmail.com>
2026-08-16 16:15:28 +0530

    Bump Android app versionCode to 27 and versionName to 27.0

--- astroapp/android/app/build.gradle.kts
+++ astroapp/android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.astrohark.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26
-        versionName = "26.0"
+        versionCode = 27
+        versionName = "27.0"
 
         ndk {
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
